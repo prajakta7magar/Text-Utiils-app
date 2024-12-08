fa78eb8eca4cdc926360be7051d36048ad89261a
Prajakta Magar <prajakta7magar@gmail.com>
2024-12-08 22:48:12 +0530

    Updates

---
+++ precache-manifest.5b58d7d147bb2031a33e2a1825fe2f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eadb4726e77b366eb6e3df7f9845c78",
+    "url": "%3Cprajakta7magar%3E.github.io/%3CText-Utiils-app%3E/index.html"
+  },
+  {
+    "revision": "15c8dd8e14e47fa99340",
+    "url": "%3Cprajakta7magar%3E.github.io/%3CText-Utiils-app%3E/static/css/main.38896bfd.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "%3Cprajakta7magar%3E.github.io/%3CText-Utiils-app%3E/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "%3Cprajakta7magar%3E.github.io/%3CText-Utiils-app%3E/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "15c8dd8e14e47fa99340",
+    "url": "%3Cprajakta7magar%3E.github.io/%3CText-Utiils-app%3E/static/js/main.58f052b1.chunk.js"
+  },
+  {
+    "revision": "0e8badeb918fef2b7e5c",
+    "url": "%3Cprajakta7magar%3E.github.io/%3CText-Utiils-app%3E/static/js/runtime~main.5a8cceb8.js"
+  }
+]);
